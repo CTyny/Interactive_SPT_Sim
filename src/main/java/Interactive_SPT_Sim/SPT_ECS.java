f5b257c54935012799c402dc8b6a8091cfd596a4
ct83 <ct83@CLFPC026.dc.clf.rl.ac.uk>
2017-02-21 12:42:04 +0000

    Main runs short test loop (10 clicks). FluorescenceSystem implemented.

--- src/main/java/Interactive_SPT_Sim/SPT_ECS.java
+++ src/main/java/Interactive_SPT_Sim/SPT_ECS.java
@@ -10,32 +10,41 @@ import com.artemis.WorldConfigurationBuilder;
 
 public class SPT_ECS {
     
-    public class world {
     //Configure world
-    //note: systems are called in the order they are added to WorldConfigurationBuilder    
+    //note: systems are called in the order they are added to WorldConfigurationBuilder
+    int chipSize = 512;
+    double pixelSize = 160;
+    double fov = 81920; //field of view size in nm
+    
     WorldConfiguration simConfig = new WorldConfigurationBuilder()
-    .with(new MotionSystem(), new FluorescenceSystem(), new BrownianSystem()).build();
-
+    .with(new MotionSystem(), new FluorescenceSystem(), new BrownianSystem(), new TrackRenderSystem()).build();
+    
     World microWorld = new World(simConfig);
-
+    
+    //create an entity to test
     int e = microWorld.create();
 
     ComponentMapper<FluorescenceComponent> fluorescence;
-    //ComponentMapper<PositionComponent> position;
-    //ComponentMapper<VelocityComponent> velocity;
+    ComponentMapper<PositionComponent> position;
+    ComponentMapper<VelocityComponent> velocity;
 
-    FluorescenceComponent fluor = fluorescence.create(e);
+    //having these here causes build failure(?!) need to figure out why!
     //PositionComponent pos = position.create(e);
     //VelocityComponent vel = velocity.create(e);
-
-    for (int j=0; j<10; j++){
-            microWorld.setDelta(j);
-            microWorld.process();
-        }
+    
+    
+    public void simLoop() {
+        microWorld.setDelta(1);
+        microWorld.process();
     }
-    public static void main (final String...args) {
-        
-        
+    
+    public static void main (final String[] args) {
+       
+        SPT_ECS sim = new SPT_ECS();
+        for (int i=0; i<10; i++){
+            sim.simLoop();
+        }
+        //new SPT_ECS().simLoop(); //never ending simulation!!!
     }
 }
 
