b1f256297228e685ae6733c7c9aeedaca55ccf82
CTyny <>
2017-03-01 16:40:39 +0000

    Empty SingleMoleculeImageGenerator class created.

---
+++ src/main/java/Interactive_SPT_Sim/SingleMoleculeImageGenerator.java
@@ -0,0 +1,6 @@
+package Interactive_SPT_Sim;
+
+public class SingleMoleculeImageGenerator {
+//TODO: method to describe 2d-gaussian comprising set number of counts
+//TODO: method to take gaussian position in 2d pixel array and allocate counts to pixels
+}
