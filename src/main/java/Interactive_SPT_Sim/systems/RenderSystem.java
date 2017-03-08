e5eccda0a36f602a0808362ca82a003bb4855cd3
CTyny <ct83@CLFPC026.dc.clf.rl.ac.uk>
2017-03-08 13:41:03 +0000

    Added IJ.wait() command to RenderSystem.

--- src/main/java/Interactive_SPT_Sim/systems/RenderSystem.java
+++ src/main/java/Interactive_SPT_Sim/systems/RenderSystem.java
@@ -7,9 +7,11 @@ import com.artemis.Aspect;
 import com.artemis.BaseEntitySystem;
 import com.artemis.ComponentMapper;
 import com.artemis.utils.IntBag;
+import ij.IJ;
 import ij.ImagePlus;
 import ij.process.ImageProcessor;
 import java.util.Arrays;
+import java.util.Random;
 
 //This system needs to perform tasks before and after iterating through entities. 
 //Extends BaseEntitySystem so that ProcessSystem can be overidden.
@@ -19,14 +21,11 @@ public class RenderSystem extends BaseEntitySystem {
     ComponentMapper<FluorescenceComponent> fluorescence;
     ComponentMapper<PositionComponent> position;
     ImagePlus outputImage;
-    int pixR;
-    int[][] pixValues;
+    int pixR;//pixel resolution of image
+    int[][] pixValues;//array to store pixel values as they are generated for each fluorescent entity
     
-    /*public RenderSystem(Aspect.Builder aspect) {
-		super(aspect);
-	}*/
     public RenderSystem (ImagePlus outputImage){
-        super(Aspect.all());
+        super(Aspect.all(FluorescenceComponent.class));//only interested in entities tagged with FluorescenceComponent
         this.outputImage = outputImage;
         pixR = outputImage.getWidth();
         pixValues = new int [pixR][pixR];
@@ -39,9 +38,10 @@ public class RenderSystem extends BaseEntitySystem {
         PositionComponent p = position.getSafe(entityId, null);
         eLoc[0] = p.x;
         eLoc[1] = p.y;
-
+        Random r = new Random();
+        int counts = (int) (1500+(r.nextGaussian()*0.5));//add some variation to total counts for each molecule - not sure this is having much impact!
         SingleMoleculeImageGenerator smig = new SingleMoleculeImageGenerator();
-        pixValues = smig.addCountsToImage(pixValues, pixR, 81920, 1500, 150, eLoc);
+        pixValues = smig.addCountsToImage(pixValues, pixR, 81920, counts, 150, eLoc);
         
         for (int i=0; i<pixR; i++) {
             for (int j=0; j <pixR; j++){
@@ -56,7 +56,7 @@ public class RenderSystem extends BaseEntitySystem {
         for (int i=0; i<pixR; i++){
             Arrays.fill(pixValues[i],0);
         }
-        //get entity IDs that thuis sytem is interested in
+        //get entity IDs that this sytem is interested in
         IntBag actives = subscription.getEntities();
         int[] ids =actives.getData();
         //Iterate through entities
@@ -64,6 +64,7 @@ public class RenderSystem extends BaseEntitySystem {
             process(ids[i]);
         }
         //update ImagePlus object
+        IJ.wait(50);//wait to update. TODO: figure out how to get world Delta then convert to millisecond wait time here
         outputImage.updateAndDraw();
     }
 }
